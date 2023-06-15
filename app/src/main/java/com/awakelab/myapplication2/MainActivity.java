6c089636e7abed959c98aac54ba85dd0aecc57ed
OskarUno <oorellana80@gmail.com>
2023-06-15 00:35:32 -0400

    Sing Up

---
+++ app/src/main/java/com/awakelab/myapplication2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.awakelab.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+
+}
